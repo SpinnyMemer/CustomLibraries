1dea3dccfb774880e69b192a8d287e0415538db0
SpinnyMemer <110652422+SpinnyMemer@users.noreply.github.com>
2025-01-17 12:12:41 +0530

    Update CustomKick.lua

--- CustomKick.lua
+++ CustomKick.lua
@@ -4,11 +4,10 @@ local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
 local CoreGui = game:GetService("CoreGui")
 
-local KickTitle = CoreGui.RobloxPromptGui.promptOverlay.ErrorPrompt.TitleFrame.ErrorTitle
-local KickBody = CoreGui.RobloxPromptGui.promptOverlay.ErrorPrompt.MessageArea.ErrorFrame.ErrorMessage
-
 function CustomKick:RemovePlayer(title, body)
     Player.Kick(Player, "")
+    local KickTitle = CoreGui.RobloxPromptGui.promptOverlay.ErrorPrompt.TitleFrame.ErrorTitle
+    local KickBody = CoreGui.RobloxPromptGui.promptOverlay.ErrorPrompt.MessageArea.ErrorFrame.ErrorMessage
     KickTitle.Text = title
     KickBody.Text = tostring(body)
 end
